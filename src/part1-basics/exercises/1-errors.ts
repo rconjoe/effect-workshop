18b46f833924e8e5186b8e61ed0a63bdf2890919
rconjoe <root@trog.codes>
2025-09-18 21:09:26 -0700

    finish errors tests

--- src/part1-basics/exercises/1-errors.ts
+++ src/part1-basics/exercises/1-errors.ts
@@ -1,4 +1,4 @@
-import { Array, Effect, Either, Option } from "effect";
+import { Array, Effect, Either, Option, Schedule } from "effect";
 import * as T from "../../testDriver.ts";
 
 /**
@@ -9,10 +9,29 @@ import * as T from "../../testDriver.ts";
 
 let i = 0;
 const eventuallySucceeds = Effect.suspend(() =>
-  i++ < 100 ? Effect.fail("error") : Effect.succeed(5)
+  i++ < 100 ? Effect.fail("error") : Effect.succeed(5),
 );
 
-const testOne = eventuallySucceeds;
+// This fails.
+// const testOne = eventuallySucceeds;
+
+// One solution is recursive. You can use matchEffect, which allows you to specify an Effect to run for both
+// onSuccess and onFailure cases. In this case, we want to match onSuccess to just returning the result, and
+// onFailure to re-running the eventually succeeding Effect.
+// const testOne: Effect.Effect<number> = Effect.suspend(() =>
+//   Effect.matchEffect(eventuallySucceeds, {
+//     onSuccess: (_) => Effect.succeed(_),
+//     onFailure: () => testOne,
+//   }),
+// );
+
+// Another solution is to retry it forever.
+// const testOne = Effect.retry(eventuallySucceeds, {times: Infinity});
+// or
+// const testOne = Effect.retry(eventuallySucceeds, Schedule.forever);
+
+// But I think this makes the most sense:
+const testOne = Effect.eventually(eventuallySucceeds);
 
 await T.testRunAssert(1, testOne, { success: 5 });
 
@@ -29,11 +48,23 @@ const maybeFailArr = Array.allocate<number>(10)
   .map((_, index) => index + 1)
   .map((number) => maybeFail(number));
 
-const testTwo = Effect.all(maybeFailArr);
+// Fails:
+// const testTwo = Effect.all(maybeFailArr);
 
-// await T.testRunAssert(2, testTwo, {
-//   failure: ["odd 1", "odd 3", "odd 5", "odd 7", "odd 9"],
-// });
+// I didn't know about this: https://effect-ts.github.io/effect/effect/Effect.ts.html#all
+// Default mode is to short-circuit on the first failure. You can change it to accumulate things
+// wither "either" (uses Either<Right, Left>) or "validate" (uses Option<None, Some>). So here
+// we use "validate" and then filter out the Errors that come back as "None", leaving an array
+// of all the accumulated Errors.
+const testTwo = Effect.all(maybeFailArr, {
+  mode: "validate",
+}).pipe(
+  Effect.mapError((errors) => errors.filter(Option.isSome).map((_) => _.value)),
+);
+
+await T.testRunAssert(2, testTwo, {
+  failure: ["odd 1", "odd 3", "odd 5", "odd 7", "odd 9"],
+});
 
 /**
  * # Exercise 3:
@@ -41,11 +72,21 @@ const testTwo = Effect.all(maybeFailArr);
  * Now `succeed` with both an array of success values and an array of errors
  */
 
-const testThree = Effect.all(maybeFailArr);
+// Here just use the "either" mode. Then, remember that "andThen" encapsulates
+// all the things you'd want to compose for the most part. Initial thought was to use
+// flatMap, but then remembered that andThen covers all cases for that.
+const testThree = Effect.all(maybeFailArr, {
+  mode: "either",
+}).pipe(
+  Effect.andThen((result) => ({
+    success: result.filter(Either.isRight).map((_) => _.right),
+    failure: result.filter(Either.isLeft).map((_) => _.left),
+  })),
+);
 
-// await T.testRunAssert(3, testThree, {
-//   success: {
-//     success: [2, 4, 6, 8, 10],
-//     failure: ["odd 1", "odd 3", "odd 5", "odd 7", "odd 9"],
-//   },
-// });
+await T.testRunAssert(3, testThree, {
+  success: {
+    success: [2, 4, 6, 8, 10],
+    failure: ["odd 1", "odd 3", "odd 5", "odd 7", "odd 9"],
+  },
+});
